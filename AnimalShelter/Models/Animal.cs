992238f0d8afe1c4795b23530e0e3093746e787c
Thomas Russell <t.p.russell19@gmail.com>
2021-05-18 15:13:38 -0700

    add files and code to add animals to the database and view them in the DOM
    Co-authored-by: Carlos Mendez <carlosmendez86@gmail.com>
    Co-Authored-by: Michael Burton <micbur1@gmail.com>

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string DOA { get; set; }
+    public string Breed { get; set; }
+    public string Type { get; set; }
+
+  }
+}
